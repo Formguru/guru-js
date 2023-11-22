4505373ca0ae0c09bb70b73c5d67d170a53e3a2f
adam-harwood <45190361+adam-harwood@users.noreply.github.com>
2023-11-22 10:29:24 +1000

    Create onnx_model.js
    This is factored out from the console, and tweaked to be usable by guru-engine.

---
+++ onnx_model.js
@@ -0,0 +1,50 @@
+import { loadModelByName } from "guru/onnxruntime.mjs";
+
+class OnnxModel {
+
+  constructor(inputName, session, metadata) {
+    this.inputName = inputName;
+    this.session = session;
+    this.metadata = metadata;
+  }
+
+  static async load(modelName, sessionOptions = { executionProviders: ['wasm'] }, metadata) {
+    const session = await loadModelByName(modelName, sessionOptions);
+    return OnnxModel.loadFromSession(session, metadata);
+  }
+
+  static loadFromSession(session, metadata) {
+    const inputName = session.inputNames[0];
+    const {inputWidth, inputHeight} = {inputWidth: 192, inputHeight: 256, ...metadata};
+
+    const _metadata = new OnnxModelMetadata(
+      new ImgNormCfg([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+      new ImgSize(inputWidth, inputHeight)
+    );
+
+    return new OnnxModel(inputName, session, _metadata);
+  }  
+}
+
+class ImgNormCfg {
+  constructor(mean, std) {
+    this.mean = mean;
+    this.std = std;
+  }
+}
+
+class ImgSize {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class OnnxModelMetadata {
+  constructor(normCfg, size) {
+    this.normCfg = normCfg;
+    this.size = size;
+  }
+}
+
+export {OnnxModel, ImgNormCfg, ImgSize, OnnxModelMetadata}
