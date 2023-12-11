218f764ce3f098b4f95278702e91256850e81b72
Rick Bhardwaj <rickbhardwaj@gmail.com>
2023-12-11 12:17:28 -0800

    Adding face detection (#7)
    This PR adds face detection using the ultraface model from https://github.com/onnx/models.

---
+++ face_detection.mjs
@@ -0,0 +1,69 @@
+import { resize } from "./preprocess.mjs";
+import { getOrt } from "./onnxruntime.mjs";
+import { tensorToMatrix } from "./inference_utils.mjs";
+
+export class UltrafaceFaceDetector {
+  /**
+   * Load the face detection model using the given ONNX model.
+   *
+   * @param {InferenceSession} onnxModel
+   */
+  constructor(onnxModel) {
+    this.model = onnxModel;
+    this.ort = getOrt();
+  }
+
+  _preprocessImage(image) {
+    return resize(image, { width: 320, height: 240 });
+  }
+
+  _normalize(image) {
+    const { width, height, numChannels } = image;
+    const chw = image.getData({ channelsFirst: true });
+    const output = new Float32Array(3 * height * width);
+
+    // if input is integer in [0,255], convert to float32 in [0,1]
+    const normFactor = 128;
+
+    const mean = [127, 127, 127];
+
+    for (let i = 0; i < chw.length; i += numChannels) {
+      output[i + 0] = (chw[i + 0] - mean[0]) / normFactor;
+      output[i + 1] = (chw[i + 1] - mean[1]) / normFactor;
+      output[i + 2] = (chw[i + 2] - mean[2]) / normFactor;
+    }
+
+    return output;
+  }
+
+  async run(img) {
+    const resizedImage = this._preprocessImage(img);
+    const modelInput = new this.ort.Tensor(
+      "float32",
+      this._normalize(resizedImage),
+      [1, 3, resizedImage.height, resizedImage.width]
+    );
+
+    const modelOutput = await this.model.run({ input: modelInput });
+    const boxesMatrix = tensorToMatrix(modelOutput.boxes);
+    const scoresMatrix = tensorToMatrix(modelOutput.scores);
+
+    const proposals = scoresMatrix[0];
+
+    var highestScore = 0;
+    var highestScoreIndex = 0;
+
+    for (let i = 0; i < proposals.length; i++) {
+      const proposal = proposals[i];
+      const score = proposal[1];
+      if (score > highestScore) {
+        highestScore = score;
+        highestScoreIndex = i;
+      }
+    }
+
+    return {
+      box: boxesMatrix[0][highestScoreIndex],
+    };
+  }
+}
